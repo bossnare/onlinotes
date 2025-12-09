5e48cde72f383144836e83ba2f1e6fe650bd7056
bossnare <razafimangagervaischristo@gmail.com>
2025-12-09 15:01:15 +0300

    client:ui: add bottom nav for mobile

--- apps/client/src/components/navigation/NavTab.tsx
+++ apps/client/src/components/navigation/NavTab.tsx
@@ -1,22 +1,31 @@
-import { Earth, Trash, Layers } from 'lucide-react';
+import { Search, BellIcon, Layers, Tags } from 'lucide-react';
 // import { NavLink } from 'react-router-dom';
 
 export function NavTab() {
   return (
     <>
       <li>
-        <button className="flex items-center w-full gap-2 px-2 font-semibold text-white bg-zinc-900 rounded-md cursor-pointer md:px-3 h-9">
-          <Earth className="size-5" /> All Notes
+        <button className="flex items-center w-full gap-2 px-2 font-semibold text-white rounded-md cursor-pointer bg-zinc-900 md:px-3 h-9">
+          <Layers className="md:size-5" />{' '}
+          <span className="hidden md:block">Overview</span>
         </button>
       </li>
       <li>
         <button className="flex items-center w-full gap-2 px-2 font-medium cursor-pointer md:px-3 md:hover:opacity-80 text-zinc-400 h-9">
-          <Layers className="size-5" /> Workspaces
+          <Search className="md:size-5" />{' '}
+          <span className="hidden md:block">Search</span>
         </button>
       </li>
       <li>
         <button className="flex items-center w-full gap-2 px-2 font-medium cursor-pointer md:px-3 md:hover:opacity-80 text-zinc-400 h-9">
-          <Trash className="size-5" /> Trash
+          <BellIcon className="md:size-5" />{' '}
+          <span className="hidden md:block">Notifications</span>
+        </button>
+      </li>
+      <li>
+        <button className="flex items-center w-full gap-2 px-2 font-medium cursor-pointer md:px-3 md:hover:opacity-80 text-zinc-400 h-9">
+          <Tags className="md:size-5" />{' '}
+          <span className="hidden md:block">Tags</span>
         </button>
       </li>
     </>
