111960168169ad7c89df208b07be284a745424c7
bossnare <razafimangagervaischristo@gmail.com>
2025-12-10 14:07:44 +0300

    client:theme: add 2 themes colors

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -30,7 +30,7 @@ function App() {
       <div className="fixed inset-y-0 z-20 hidden w-64 overflow-y-auto border-r md:block border-zinc-800">
         <aside className="relative px-2 space-y-1 size-full">
           <Logo className="sticky top-0 hidden w-full py-3 bg-black/80 backdrop-blur-sm md:flex" />
-          <nav className="pb-2 rounded-md bg-zinc-950">
+          <nav className="pb-2 rounded-md bg-muted">
             <ul className="flex flex-col space-y-2">
               <NavTab />
             </ul>
@@ -81,9 +81,9 @@ function App() {
 
         <main className="h-[calc(100dvh-3.5rem)] bg-linear-to-b from-zinc-950/50 to-zinc-950/20 overflow-y-auto px-2 grid grid-cols-4 items-start gap-2">
           <div className="flex justify-center py-6 col-span-full">
-            <div className="flex flex-col items-center justify-center w-full gap-4 p-10 rounded-lg md:w-2/3 bg-zinc-950">
+            <div className="flex flex-col items-center justify-center w-full gap-4 p-10 rounded-lg md:w-2/3 bg-muted">
               <h4 className="text-lg font-black">Browse your notes</h4>
-              <p className="text-sm text-center text-zinc-500">
+              <p className="text-sm text-center text-muted-foreground">
                 If you click this browse button, your app system down instead,
                 and keep click, calm. As a JS dev, click this button please.
               </p>
