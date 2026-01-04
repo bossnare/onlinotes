d07395abb1e5ffde50722b79072570a8cb3a0205
bossnare <razafimangagervaischristo@gmail.com>
2026-01-04 11:53:23 +0300

    client:ux: drop shadow card notes on mobile breakpoint

--- apps/client/src/app/components/navigation/NavTab.tsx
+++ apps/client/src/app/components/navigation/NavTab.tsx
@@ -1,13 +1,16 @@
 import { NavLink } from 'react-router-dom';
 import { tabLabel } from './label';
 import { Tab } from './Tab';
+import { useLayoutStore } from '@/app/stores/layoutStore';
 
 export function NavTab() {
+  const isOpenPanel = useLayoutStore((s) => s.isOpenPanel);
+
   return (
     <>
       {tabLabel.map((t) => (
-        <li key={t.id}>
-          <NavLink title={t.label} to={t.route} end={t.route === '/app'}>
+        <li title={!isOpenPanel ? t.label : ''} key={t.id}>
+          <NavLink to={t.route} end={t.route === '/app'}>
             {({ isActive }) => (
               <Tab isActive={isActive} Icon={t.icon} label={t.label} />
             )}
--- apps/client/src/app/page/Overview.tsx
+++ apps/client/src/app/page/Overview.tsx
@@ -10,7 +10,7 @@ function Overview() {
 
   return (
     <>
-      <div className="py-4 px-3 md:px-4 space-y-2 min-h-screen bg-muted dark:bg-background">
+      <div className="px-3 md:px-4 space-y-2 min-h-screen bg-muted dark:bg-background">
         {/* <div className="relative flex flex-col w-full gap-2 p-4 rounded-lg shadow-xs md:p-3 bg-muted dark:bg-muted/60">
           <h4 className="font-bold">Complete your profile</h4>
           <div className="flex flex-col justify-center gap-3 md:items-center md:flex-row md:justify-between">
@@ -39,14 +39,14 @@ function Overview() {
           </div>
         ) : (
           <>
-            <header>
-              <h3 className="text-2xl font-semibold tracking-tight scroll-m-20">
+            <header className="px-1 pt-8">
+              <h3 className="text-2xl font-medium tracking-tight scroll-m-20">
                 All notes
               </h3>
             </header>
             <div className="grid grid-cols-2 md:grid-cols-4 pt-4 gap-3 flex-wrap">
               {me?.data?.notes.map((n) => (
-                <div className="bg-background dark:bg-muted/50 shadow-sm min-h-30 flex flex-col gap-4 rounded-lg p-4">
+                <div className="bg-background dark:bg-muted/50 md:shadow-sm min-h-30 flex flex-col gap-4 rounded-3xl lg:rounded-xl p-4">
                   <span className="text-lg md:text-base font-bold truncate line-clamp-2">
                     {n.title || 'Untitled'}
                   </span>
