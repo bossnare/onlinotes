11de64e2415647f52d5ab6bfb8d8483b601c5404
bossnare <razafimangagervaischristo@gmail.com>
2026-01-07 10:54:20 +0300

    client:feat: add shot by feat for notes(mobile only shorting)

--- apps/client/src/app/api/notes.api.ts
+++ apps/client/src/app/api/notes.api.ts
@@ -2,16 +2,23 @@ import { fetcher } from '@/app/lib/fetcher';
 import type { NoteInterface } from '@/app/types/note.interface';
 
 import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { useSearchParams } from 'react-router-dom';
 
 export function useNote() {
+  const [searchParams] = useSearchParams();
+  const sort = searchParams.get('sort') ?? 'createdAt';
+  const order = searchParams.get('order') ?? 'desc';
+  const params = new URLSearchParams();
+  params.set('sort', sort);
+  params.set('order', order);
+
   return useQuery<NoteInterface[]>({
-    queryKey: ['notes'],
+    queryKey: ['notes', sort, order],
     queryFn: async () => {
-      const res = await fetcher('/notes');
+      const res = await fetcher(`/notes?${params}`);
       return res.data; // return {.., data}
     },
     staleTime: 0,
-    // cacheTime: 1000 * 60 * 5, // 5 min
     gcTime: 2 * 60 * 100, // 2 min
     refetchOnWindowFocus: true,
   });
