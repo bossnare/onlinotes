bb51bdcad8bad250bea4aa8b1fe6cb6a85210c29
bossnare <razafimangagervaischristo@gmail.com>
2026-01-14 09:59:40 +0300

    server:fix: entrypoint.sh, use npx/node for migration deploy and starter app

--- apps/server/entrypoint.sh
+++ apps/server/entrypoint.sh
@@ -2,8 +2,8 @@
 
 # Run migration (prod)
 echo "Running Prisma migrations..."
-bunx prisma migrate deploy
+npx prisma migrate deploy
 
 # Run the prod app
 echo "Starting NestJS app..."
-bun run start:prod
+node dist/src/main.js
